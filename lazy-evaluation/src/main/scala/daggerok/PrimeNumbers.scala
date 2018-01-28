79d9a7e6be4e74e3bd5eb4c53a34b03e841ce1db
Maksim Kostromin <daggerok@gmail.com>
2018-01-28 23:19:46 +0200

    Initial commit

---
+++ lazy-evaluation/src/main/scala/daggerok/PrimeNumbers.scala
@@ -0,0 +1,11 @@
+package daggerok
+
+class PrimeNumbers {
+
+  def of(ns: Stream[Int]): Stream[Int] =
+    ns.head #::
+      of(ns.tail.filter(n => n % ns.head != 0))
+
+  def streamNumbers(n: Int): Stream[Int] =
+    n #:: streamNumbers(n + 1)
+}
