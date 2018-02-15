8a38d24e3d37fdc56ed5135e6e5e2f129055a314
Maksim Kostromin <daggerok@gmail.com>
2018-02-15 20:10:15 +0200

    remove useless not working maven from first-akka-blood

--- first-akka-blood/src/main/scala/daggerok/App.scala
+++ first-akka-blood/src/main/scala/daggerok/App.scala
@@ -2,7 +2,7 @@ package daggerok
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.persistence.PersistentActor
-import akka.routing.FromConfig
+import com.typesafe.config.ConfigFactory
 
 case object IncrementCommand
 
@@ -34,11 +34,16 @@ class CounterActor extends PersistentActor {
 object App {
 
   def main(args: Array[String]) {
-    // val system = ActorSystem("ap")
-    val system = ActorSystem.create("ap")
-    val config = FromConfig.props(Props[CounterActor])
-    val persistentActor: ActorRef = system.actorOf(config, name = "counter")
-    //// TODO
-    // persistentActor ! IncrementCommand
+    println(s"\n\n${args.length}\n\n")
+    val config = ConfigFactory.load()
+    val system = ActorSystem("ap")
+//    val system = ActorSystem("ap", config.getConfig("myapp"))
+//    val system = ActorSystem("ap", config.getConfig("myapp").withFallback(config))
+    //val system = ActorSystem.create("ap")
+    val persistentActor: ActorRef = system.actorOf(Props(classOf[CounterActor]), "counter")
+    val amount = if (args.length > 0) args(1).toInt else 1
+    for ( _ <- 0 to amount) {
+      persistentActor ! IncrementCommand
+    }
   }
 }
